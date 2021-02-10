96205b1dc0ae0c74a33a320022653ead4a887fcc
Laptop@ikaushikpal <iamkaushik@Laptop.com>
2021-02-10 12:24:29 +0530

    practice sesion

---
+++ practice/flask01.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    print("working")
+    return "hello world"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5500)
